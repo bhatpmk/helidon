e2381126c5f92a5e1cc6371ead5556af2688c497
Laird Nelson <laird.nelson@oracle.com>
2026-04-15 21:08:37 -0700

    Squashable commit; introduces simple generic argument processing in JdbcPlan for cases where generating a binder is too burdensome
    Signed-off-by: Laird Nelson <laird.nelson@oracle.com>

--- data/plan/jdbc/src/main/java/io/helidon/data/plan/jdbc/JdbcPlan.java
+++ data/plan/jdbc/src/main/java/io/helidon/data/plan/jdbc/JdbcPlan.java
@@ -27,6 +27,8 @@ import io.helidon.data.jdbc.JdbcResults;
 import io.helidon.data.jdbc.function.JdbcConsumer;
 import io.helidon.data.jdbc.function.JdbcSupplier;
 
+import static java.util.Arrays.asList;
+
 /**
  * An executable plan for the presumed execution of a statement using JDBC constructs.
  *
@@ -47,7 +49,7 @@ public interface JdbcPlan extends RuntimeType.Api<JdbcPlanConfig> {
                         JdbcConsumer<? super JdbcPreparedStatementBindingView> argsBinder)
         throws SQLException;
 
-    /**
+    /*
      * Executes this plan, returning a {@link JdbcResults}.
      *
      * @param cs a non-{@code null} {@link JdbcSupplier} of a non-{@code null} {@link Connection}
@@ -56,8 +58,26 @@ public interface JdbcPlan extends RuntimeType.Api<JdbcPlanConfig> {
      * @throws SQLException if a database error occurs
      * @see #execute(JdbcSupplier, JdbcConsumer)
      */
-    default JdbcResults execute(JdbcSupplier<? extends Connection> cs) throws SQLException {
-        return this.execute(cs, JdbcPlanImpl::doNothing);
+    // default JdbcResults execute(JdbcSupplier<? extends Connection> cs) throws SQLException {
+    //     return this.execute(cs, JdbcPlanImpl::doNothing);
+    // }
+
+    /**
+     * Executes this plan, returning a {@link JdbcResults}.
+     *
+     * @param cs aa non-{@code null} {@link JdbcSupplier} of a non-{@code null} {@link Connection}
+     * @param args arguments
+     * @return a non-{@code null} {@link JdbcResults}; <strong>callers must {@linkplain JdbcResults#close() close} it
+     * when finished</strong>
+     * @throws SQLException if a database error occurs
+     * @see #execute(JdbcSupplier, JdbcConsumer)
+     */
+    default JdbcResults execute(JdbcSupplier<? extends Connection> cs, Object... args) throws SQLException {
+        if (args == null || args.length == 0) {
+            return this.execute(cs, JdbcPlanImpl::doNothing);
+        }
+        GenericBinder b = new GenericBinder();
+        return this.execute(cs, psView -> b.bind(psView, asList(args)));
     }
 
     /**
@@ -97,7 +117,7 @@ public interface JdbcPlan extends RuntimeType.Api<JdbcPlanConfig> {
 
     /**
      * A convenience method that arranges for the supplied {@code statement}, requiring no arguments, to be {@linkplain
-     * #execute(JdbcSupplier)} executed.
+     * #execute(JdbcSupplier, Object...)} executed.
      *
      * <p>This method:</p>
      *
@@ -105,7 +125,7 @@ public interface JdbcPlan extends RuntimeType.Api<JdbcPlanConfig> {
      * <li>Invokes the {@link #builder()} method</li>
      * <li>Invokes its {@link JdbcPlanConfig.Builder#statement(String)} method with the supplied {@code statement}</li>
      * <li>Invokes the {@link JdbcPlanConfig.Builder#build()} method to build a minimally-configured {@link JdbcPlan}</li>
-     * <li>Invokes the {@link #execute(JdbcSupplier)} method with the supplied {@link JdbcSupplier}</li>
+     * <li>Invokes the {@link #execute(JdbcSupplier, Object...)} method with the supplied {@link JdbcSupplier}</li>
      * <li>Returns the results</li>
      * </ol>
      *
@@ -117,16 +137,16 @@ public interface JdbcPlan extends RuntimeType.Api<JdbcPlanConfig> {
      * @see #builder()
      * @see JdbcPlanConfig.Builder#statement(String)
      * @see JdbcPlanConfig.Builder#build()
-     * @see #execute(JdbcSupplier)
+     * @see #execute(JdbcSupplier, Object...)
      * @see JdbcResults
      */
     static JdbcResults execute(JdbcSupplier<? extends Connection> cs, String statement) throws SQLException {
         return builder().statement(statement).build().execute(cs);
     }
 
-        /**
+    /**
      * A convenience method that arranges for the supplied {@code statement}, requiring no arguments, to be {@linkplain
-     * #execute(JdbcSupplier)} executed.
+     * #execute(JdbcSupplier, Object...)} executed.
      *
      * <p>This method:</p>
      *
@@ -134,7 +154,8 @@ public interface JdbcPlan extends RuntimeType.Api<JdbcPlanConfig> {
      * <li>Invokes the {@link #builder()} method</li>
      * <li>Invokes its {@link JdbcPlanConfig.Builder#statement(String)} method with the supplied {@code statement}</li>
      * <li>Invokes the {@link JdbcPlanConfig.Builder#build()} method to build a minimally-configured {@link JdbcPlan}</li>
-     * <li>Invokes the {@link #execute(JdbcSupplier)} method with {@link DataSource#getConnection() ds::getConnection}</li>
+     * <li>Invokes the {@link #execute(JdbcSupplier, Object...)} method with {@link DataSource#getConnection()
+     * ds::getConnection}</li>
      * <li>Returns the results</li>
      * </ol>
      *
@@ -146,7 +167,7 @@ public interface JdbcPlan extends RuntimeType.Api<JdbcPlanConfig> {
      * @see #builder()
      * @see JdbcPlanConfig.Builder#statement(String)
      * @see JdbcPlanConfig.Builder#build()
-     * @see #execute(JdbcSupplier)
+     * @see #execute(JdbcSupplier, Object...)
      * @see JdbcResults
      */
     static JdbcResults execute(DataSource ds, String statement) throws SQLException {
